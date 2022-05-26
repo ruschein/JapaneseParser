ca41ec3497b689da14f00fae903e7b1bb93bc7dc
Dr. Johannes Ruscheinski <ruschein@protonmail.com>
2022-05-26 17:11:53 +0200

    Buggy! Handle possible suffixes that are sequences of characters.

--- JapaneseParser.py
+++ JapaneseParser.py
@@ -3,11 +3,31 @@
 import sys
 
 
+# Strips "possible_suffix" from "reversed_sentence", if "reversed_sentence" ends
+# with "possible_suffix".
+# @return "possible_suffix" if "reversed_sentence" ends w/ "possible_suffix" o/w False.
+def StripIfEndsWith(reversed_sentence, possible_suffix):
+    if len(reversed_sentence) >= len(possible_suffix) \
+       and reversed_sentence[:len(possible_suffix)] == possible_suffix:
+        reversed_sentence = reversed_sentence[len(possible_suffix):]
+        print("possible_suffix = " + possible_suffix + ", reversed_sentence = "+reversed_sentence)
+        return possible_suffix
+    return False
+
+
 def StripSentenceEnders(reversed_sentence, reversed_components):
     sentence_ender_particles = ['か', 'ね', 'よ', 'な', 'の']
-    while reversed_sentence and reversed_sentence[0] in sentence_ender_particles:
-        reversed_components.append(reversed_sentence[0])
-        reversed_sentence = reversed_sentence[1:]
+    while True:
+        matched_at_least_one = False
+        for sentence_ender_particle in sentence_ender_particles:
+            stripped_ender = StripIfEndsWith(reversed_sentence, sentence_ender_particle[::-1])
+            print(str(stripped_ender) + ", " + reversed_sentence)
+            if stripped_ender:
+                reversed_components.append(stripped_ender)
+                matched_at_least_one = True
+        sys.exit(0)
+        if not matched_at_least_one:
+            break
 
         
 def ParseSentence(sentence):
