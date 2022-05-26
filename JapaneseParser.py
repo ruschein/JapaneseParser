79d5db28490e0126b8b302f3281c1acaa830805b
Dr. Johannes Ruscheinski <ruschein@protonmail.com>
2022-05-26 18:22:07 +0200

    Fixed a bug.

--- JapaneseParser.py
+++ JapaneseParser.py
@@ -5,14 +5,13 @@ import sys
 
 # Strips "possible_suffix" from "reversed_sentence", if "reversed_sentence" ends
 # with "possible_suffix".
-# @return "possible_suffix" if "reversed_sentence" ends w/ "possible_suffix" o/w False.
+# @return ("reversed_sentence","possible_suffix") if "reversed_sentence" ends w/ "possible_suffix" o/w ("reversed_sentence",False).
 def StripIfEndsWith(reversed_sentence, possible_suffix):
     if len(reversed_sentence) >= len(possible_suffix) \
        and reversed_sentence[:len(possible_suffix)] == possible_suffix:
         reversed_sentence = reversed_sentence[len(possible_suffix):]
-        print("possible_suffix = " + possible_suffix + ", reversed_sentence = "+reversed_sentence)
-        return possible_suffix
-    return False
+        return reversed_sentence, possible_suffix
+    return reversed_sentence, False
 
 
 def StripSentenceEnders(reversed_sentence, reversed_components):
@@ -20,12 +19,10 @@ def StripSentenceEnders(reversed_sentence, reversed_components):
     while True:
         matched_at_least_one = False
         for sentence_ender_particle in sentence_ender_particles:
-            stripped_ender = StripIfEndsWith(reversed_sentence, sentence_ender_particle[::-1])
-            print(str(stripped_ender) + ", " + reversed_sentence)
+            reversed_sentence, stripped_ender = StripIfEndsWith(reversed_sentence, sentence_ender_particle[::-1])
             if stripped_ender:
                 reversed_components.append(stripped_ender)
                 matched_at_least_one = True
-        sys.exit(0)
         if not matched_at_least_one:
             break
 
