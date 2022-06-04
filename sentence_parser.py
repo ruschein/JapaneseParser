b06a16d90c460f1a596fb11e85738e80d0aa6d85
Dr. Johannes Ruscheinski <ruschein@protonmail.com>
2022-06-04 20:06:18 +0200

    Various fixes.

--- sentence_parser.py
+++ sentence_parser.py
@@ -15,7 +15,7 @@ class SentenceComponent(Enum):
 #         or None if the end of the XML stream was reached.
 def ParseSentences(xml_stream_reader: XMLStreamReader):
   # Skip to next "sentence" opening tag:
-  while True:
+  while not xml_stream_reader.isEndDocument():
     xml_stream_reader.next()
     if xml_stream_reader.isStartElement() \
        and xml_stream_reader.getLocalName() == "sentence":
@@ -23,42 +23,50 @@ def ParseSentences(xml_stream_reader: XMLStreamReader):
     if xml_stream_reader.isEndElement() \
        and xml_stream_reader.getLocalName() == "sentences":
       return None
-
+  if xml_stream_reader.isEndDocument():
+    sys.exit("unexpected end of document! (1)")
+  
   # Extract the original Japanese sentence:
-  while True:
+  while not xml_stream_reader.isEndDocument():
     xml_stream_reader.next()
     if xml_stream_reader.isStartElement() \
        and xml_stream_reader.getLocalName() == "original":
       break
+  if xml_stream_reader.isEndDocument():
+    sys.exit("unexpected end of document! (2)")
+  xml_stream_reader.next()
   if not xml_stream_reader.isCharacters():
     sys.exit("expected characters after \"original\" opening tag!")
   japanese_sentence = xml_stream_reader.getText()
 
   # Extract the translated English sentence:
-  while True:
+  while not xml_stream_reader.isEndDocument():
     xml_stream_reader.next()
     if xml_stream_reader.isStartElement() \
        and xml_stream_reader.getLocalName() == "english":
       break
+  if xml_stream_reader.isEndDocument():
+    sys.exit("unexpected end of document! (3)")
+  xml_stream_reader.next()
   if not xml_stream_reader.isCharacters():
     sys.exit("expected characters after \"english\" opening tag!")
   english_sentence = xml_stream_reader.getText()
 
   # Extract the expected sentence parts:
-  while True:
+  while not xml_stream_reader.isEndDocument():
     xml_stream_reader.next()
     if xml_stream_reader.isStartElement() \
        and xml_stream_reader.getLocalName() == "parts":
       break
   sentence_parts = []
-  while True:
+  while not xml_stream_reader.isEndDocument():
     xml_stream_reader.next()
     if xml_stream_reader.isEndElement() \
        and xml_stream_reader.getLocalName() == "parts":
       break
     if xml_stream_reader.isStartElement() \
        and xml_stream_reader.getLocalName() == "part":
-      if xml_stream_reader.getAttributeCount() != 1 or xml_stream_reader.getAttributeLocalName() != "type":
+      if xml_stream_reader.getAttributeCount() != 1 or xml_stream_reader.getAttributeLocalName(0) != "type":
         sys.exit("expected the \"part\" tag to have exactly one attribute named \"type\"!")
       part_type = xml_stream_reader.getAttributeValue(0)
       xml_stream_reader.next()
