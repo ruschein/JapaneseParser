727e5277a481b47fe27eb86016ab796452b30740
Dr. Johannes Ruscheinski <ruschein@protonmail.com>
2022-06-06 22:15:18 +0200

    Added list of te-form endings.

--- JapaneseParser.py
+++ JapaneseParser.py
@@ -7,6 +7,7 @@ LOGICAL_PARTICLES = ['が', 'を', 'へ', 'に', 'で']
 NONLOGICAL_PARTICLES = ['は', 'の']
 POSSIBLE_ADJECTIVE_ENDINGS = ['い']
 VERB_INFINITIVE_ENDINGS = ['う', 'く', 'す', 'つ', 'ぬ', 'ぶ', 'む', 'る']
+TE_ENDINGS = ['って', 'んで', 'いて', 'いで', 'して']
 
 
 def IsHiragana(CHAR) -> bool:
